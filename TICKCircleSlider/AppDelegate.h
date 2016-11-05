ba2a7bdc3c61a5db481898f2d3e73267afaa08b6
tickCoder <tickCoder@users.noreply.github.com>
2016-11-05 22:44:29 +0800

    添加工程文件

---
+++ TICKCircleSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TICKCircleSlider
+//
+//  Created by Claris on 2016.11.05.
+//  Copyright © 2016 Claris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TICKCircleSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TICKCircleSlider
+//
+//  Created by Claris on 2016.11.05.
+//  Copyright © 2016 Claris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
